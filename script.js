4c4faa01b1dfac07c54013ef22a1653545ee5b81
Prof-Luis1986 <luisalfonsomartinez@cetis50cdmx.com>
2023-12-19 13:54:38 -0600

    Las imagenes se convirtieron a url

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-document.getElementById("hablar").addEventListener("click",()=>{decir(document.getElementById("texto").value)
-})
+// Evento de clic en el botón "decir"
+document.getElementById("hablar").addEventListener("click", () => {
+    decir(document.getElementById("texto").value);
+});
 
-function decir(texto){
-speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
-texto.pitch=2;
-texto.voices[7];
+// Función para realizar la síntesis de voz
+function decir(texto) {
+    // Crear un objeto de síntesis de voz y establecer el texto a hablar
+    const utterance = new SpeechSynthesisUtterance(texto);
+    
+    // Realizar la síntesis de voz
+    speechSynthesis.speak(utterance);
+    
+    // Ajustar el tono y la voz (nota: esta parte del código no funciona correctamente)
+    texto.pitch = 2;
+    texto.voices[7];
 }
